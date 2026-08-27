f9b72304c53411ecdbd00cefdf27a9593a58cc85
antharas730203 <antharas730203@gmail.com>
2026-08-28 00:24:33 +0800

    Delete app directory

--- app/src/main/java/tw/v2scanner/ScanAlarmReceiver.kt
+++
@@ -1,9 +0,0 @@
-package tw.v2scanner
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-class ScanAlarmReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        ScanWorker.run(context) { }
-    }
-}
