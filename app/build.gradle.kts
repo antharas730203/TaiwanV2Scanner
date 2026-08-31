ae339fea5d7104f64708bd039ff2d188ece19a64
antharas730203 <antharas730203@gmail.com>
2026-08-31 09:22:07 +0800

    release: set real app version to 0.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "tw.v2scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.6.5"
+        versionCode = 11
+        versionName = "0.6.6"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
