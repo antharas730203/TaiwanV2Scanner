a135874a9fcaece204468038faf888d5a9d93f88
antharas730203 <antharas730203@gmail.com>
2026-09-09 02:17:55 +0800

    chore: prepare V0.8.2 UI update build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "tw.v2scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
+        versionCode = 18
         versionName = "0.8.2"
     }
     buildTypes {
