1ecfa9a45f4e6e808cc0e6729edb64b80b6df58d
antharas730203 <antharas730203@gmail.com>
2026-09-10 12:47:30 +0800

    Fix V0.8.3 AppCompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,6 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
