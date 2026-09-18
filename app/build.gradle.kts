2e98ca19219da0e7666276366ba5c1bdc976d7a9
antharas730203 <antharas730203@gmail.com>
2026-09-18 16:12:28 +0800

    Bump version to V0.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "tw.v2scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.8.5"
+        versionCode = 22
+        versionName = "0.8.6"
     }
     buildTypes {
         release {
